1e2fd80d435462706c9b74d6b6bf3a3b0805ff64
canifty <can-dindar@outlook.com>
2024-11-04 10:31:52 +0100

    Initial Commit

---
+++ Challange2/Challange2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challange2App.swift
+//  Challange2
+//
+//  Created by Can Dindar on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Challange2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challange2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challange2
+//
+//  Created by Can Dindar on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
