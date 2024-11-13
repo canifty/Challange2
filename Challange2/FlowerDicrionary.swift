45b219ad4db23fec6a9f82bcc2f74900bb9a0461
francescoromeo24 <francesco24602@outlook.it>
2024-11-13 11:09:59 +0100

    create plantInfoView and FlowerDictionary. Add Plant infos on dictionary

---
+++ Challange2/FlowerDicrionary.swift
@@ -0,0 +1,8 @@
+//
+//  FlowerDicrionary.swift
+//  Challange2
+//
+//  Created by Francesco Romeo on 13/11/24.
+//
+
+import Foundation
---
+++ Challange2/PlantInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  PlantInfoView.swift
+//  Challange2
+//
+//  Created by Francesco Romeo on 11/11/24.
+//
+
+import SwiftUI
+
+struct PlantInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlantInfoView()
+}
