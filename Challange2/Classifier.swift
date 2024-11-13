80df90097ec514f5b6a45a486c15997cd935879a
canifty <can-dindar@outlook.com>
2024-11-13 09:21:21 +0100

    vision framework added

---
+++ Challange2/Classifier.swift
@@ -0,0 +1,18 @@
+//
+//  Classifier.swift
+//  Challange2
+//
+//  Created by Can Dindar on 12/11/24.
+//
+
+import SwiftUI
+
+struct Classifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Classifier()
+}
---
+++ Challange2/ImageClassifier.swift
@@ -0,0 +1,18 @@
+//
+//  ImageClassifier.swift
+//  Challange2
+//
+//  Created by Can Dindar on 12/11/24.
+//
+
+import SwiftUI
+
+struct ImageClassifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageClassifier()
+}
