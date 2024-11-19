b1a1d82be2bd03d3845cbb245869e8b81f9d157e
canifty <can-dindar@outlook.com>
2024-11-19 11:51:16 +0100

    Second View Added

---
+++ Challange2/Views/SecondLevel.swift
@@ -0,0 +1,18 @@
+//
+//  SecondLevel.swift
+//  Challange2
+//
+//  Created by Can Dindar on 19/11/24.
+//
+
+import SwiftUI
+
+struct SecondLevel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondLevel()
+}
